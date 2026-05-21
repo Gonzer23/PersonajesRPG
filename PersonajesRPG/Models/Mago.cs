74142766eff1958b566ef6707e3a0d0eebd8b6e9
Dilan López <lopezcitalandilanalexander@gmail.com>
2026-05-21 08:11:13 -0600

    Creacion de Modelos Utilizados en el programa

---
+++ PersonajesRPG/Models/Mago.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonajesRPG.Models;
+
+public class Mago : Personaje
+{
+    [Required(ErrorMessage = "La escuela de magia es obligatoria")]
+    public string EscuelaMagia { get; set; } = string.Empty;
+
+    [Range(1, 10000, ErrorMessage = "El mana debe ser mínimo 1")]
+    public int Mana { get; set; } = 1;
+}
---
+++ PersonajesRPG/Models/Personaje.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PersonajesRPG.Models;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "$tipo")]
+[JsonDerivedType(typeof(Guerrero), "Guerrero")]
+[JsonDerivedType(typeof(Mago), "Mago")]
+[JsonDerivedType(typeof(Arquero), "Arquero")]
+public abstract class Personaje
+{
+    [Required(ErrorMessage = "El nombre del personaje es obligatorio")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Range(1, 99, ErrorMessage = "El nivel debe estar entre 1 y 99")]
+    public int Nivel { get; set; } = 1;
+
+    [Range(1, 10000, ErrorMessage = "Los puntos de vida deben ser mínimo 1")]
+    public int PuntosVida { get; set; } = 1;
+
+    [Range(0, int.MaxValue, ErrorMessage = "La experiencia debe ser un número positivo")]
+    public int Experiencia { get; set; }
+}

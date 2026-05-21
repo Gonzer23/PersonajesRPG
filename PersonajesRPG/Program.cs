ade46b7318cc25371ea53cead64c24efb22c2292
Dilan López <lopezcitalandilanalexander@gmail.com>
2026-05-21 08:19:39 -0600

    Listado de personajes registrados

--- PersonajesRPG/Program.cs
+++ PersonajesRPG/Program.cs
@@ -1,6 +1,7 @@
 using PersonajesRPG.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<PersonajesRPG.Services.PersonajeJsonService>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
